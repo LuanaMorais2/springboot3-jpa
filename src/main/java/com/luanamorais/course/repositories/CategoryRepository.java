4bbfc7412882d8a67d72dba0febbbad28c5040ca
LuanaMorais2 <luana.morais2@outlook.com>
2023-04-19 18:15:10 -0300

    Category

---
+++ src/main/java/com/luanamorais/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.luanamorais.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luanamorais.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
