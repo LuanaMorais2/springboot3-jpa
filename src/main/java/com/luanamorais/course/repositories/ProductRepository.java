ddbc9bcb975475455f95fd0211bcc9ef4540d798
LuanaMorais2 <luana.morais2@outlook.com>
2023-04-19 18:39:04 -0300

    Product

---
+++ src/main/java/com/luanamorais/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.luanamorais.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luanamorais.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
