89499b0a8dca917d459fb08cfe0755007e2e4cc7
LuanaMorais2 <luana.morais2@outlook.com>
2023-04-21 22:15:11 -0300

    Feature - OrderItem, many-to-many association with extra attributes

--- src/main/java/com/luanamorais/course/entities/Order.java
+++ src/main/java/com/luanamorais/course/entities/Order.java
@@ -2,8 +2,11 @@ package com.luanamorais.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.luanamorais.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
@@ -12,27 +15,30 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone ="GMT")
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	private Integer orderStatus;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+
 	public Order() {
 	}
 
@@ -59,7 +65,6 @@ public class Order implements Serializable{
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-	
 
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
@@ -85,6 +90,11 @@ public class Order implements Serializable{
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
+	}
+
+	@JsonIgnore
+	public Set<OrderItem> getItems() {
+		return items;
 	}
 
 	@Override
---
+++ src/main/java/com/luanamorais/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.luanamorais.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luanamorais.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
+}
